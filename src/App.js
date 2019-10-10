d8022254d7d8674030893c5c07599e0275acfcda
Chandan Kumar <cksharma122@gmail.com>
2019-10-10 20:28:50 +0530

    Add multiple timezones

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import timezonesData from '../src/data/timezones';
+import moment from 'moment-timezone'
 
 const App = () => {
+  const [time, setTime] = useState(moment().tz("India/Delhi"))
   const timezones = [];
   timezonesData.forEach(timezone => {
     timezones.push.apply(timezones, timezone.utc);
   })
 
+  const updateTime = (timezone) => {
+    setTime(moment.tz(timezone));
+  }
+
   const uniqueTimezones = [...new Set(timezones)].sort();
 
   return (
     <div>
-      <select>
+      <select onChange={(e) => updateTime(e.target.value)}>
         {uniqueTimezones.map(timezone => {
           return <option value={timezone} key={timezone}>{timezone}</option>
         })}
       </select>
+      <div>
+        {time.toString()}
+      </div>
     </div>
   )
 }
