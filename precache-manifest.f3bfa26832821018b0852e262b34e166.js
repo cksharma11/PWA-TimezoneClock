7bb1b68669b92b64e81afdf9e4962bc9dd5d3cc0
Chandan Kumar <cksharma122@gmail.com>
2019-10-10 23:36:20 +0530

    Updates

---
+++ precache-manifest.f3bfa26832821018b0852e262b34e166.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae49553089d01e30b9f76bdbef4958b2",
+    "url": "/PWA-TimezoneClock/index.html"
+  },
+  {
+    "revision": "60768c8a9221b39f1489",
+    "url": "/PWA-TimezoneClock/static/css/main.54becbaf.chunk.css"
+  },
+  {
+    "revision": "27a976d4cccb83eaa19a",
+    "url": "/PWA-TimezoneClock/static/js/2.3456eb72.chunk.js"
+  },
+  {
+    "revision": "60768c8a9221b39f1489",
+    "url": "/PWA-TimezoneClock/static/js/main.393e7913.chunk.js"
+  },
+  {
+    "revision": "f0f097f79a00525c4a66",
+    "url": "/PWA-TimezoneClock/static/js/runtime-main.ac44bff3.js"
+  }
+]);
